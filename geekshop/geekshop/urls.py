e3605b46707f7ad764efeec7bc905dc5ab6c06c5
Aleksey Sheludchenkov <aleshkashell@gmail.com>
2019-03-24 20:16:12 +0300

    create handlers and views

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+import mainapp.views as mainapp
 
 urlpatterns = [
+    path('', mainapp.main),
+    path('products/', mainapp.products),
+    path('contact/', mainapp.contact),
     path('admin/', admin.site.urls),
 ]
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def main(request):
+    return render(request, 'mainapp/index.html')
+
+
+def products(request):
+    return render(request, 'mainapp/catalog.html')
+
+
+def contact(request):
+    return render(request, 'mainapp/contacts.html')
