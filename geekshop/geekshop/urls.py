c823fae39171af39e81544f52f5ee19de22c6f17
Aleksey Sheludchenkov <aleshkashell@gmail.com>
2019-03-27 15:36:00 +0300

    create base html template

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -18,8 +18,8 @@ from django.contrib import admin
 import mainapp.views as mainapp
 
 urlpatterns = [
-    path('', mainapp.main),
-    path('products/', mainapp.products),
-    path('contact/', mainapp.contact),
+    path('', mainapp.main, name='main'),
+    path('products/', mainapp.products, name='products'),
+    path('contact/', mainapp.contact, name='contact'),
     path('admin/', admin.site.urls),
 ]
