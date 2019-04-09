7e364469a33a03318fe330a81665cbc2c0a5f60e
Aleksey Sheludchenkov <aleshkashell@gmail.com>
2019-04-09 11:08:16 +0300

    Create basket view

--- geekshop/basketapp/views.py
+++ geekshop/basketapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect, get_object_or_404
 from basketapp.models import Basket
 from mainapp.models import Product
+import json
 
 main_links_menu = [
         {'href': 'main', 'name': 'Главная'},
@@ -10,16 +11,24 @@ main_links_menu = [
 
 def basket(request):
     basket = Basket.objects.filter(user=request.user)
+    products = []
+    for product in basket:
+        position = get_object_or_404(Product, pk=product.pk)
+        products.append({
+            'basket': product,
+            'product': position
+        })
     content = {
-        'basket': basket,
-        'links_menu': main_links_menu
+        'basket': products,
+        'links_menu': main_links_menu,
+
     }
     return render(request, 'basketapp/basket.html', content)
 
-    
+
 def basket_add(request, pk):
     product = get_object_or_404(Product, pk=pk)
-    
+
     basket = Basket.objects.filter(user=request.user, product=product).first()
 
     if not basket:
@@ -27,10 +36,10 @@ def basket_add(request, pk):
 
     basket.quantity += 1
     basket.save()
-    
+
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
-    
-    
+
+
 def basket_remove(request, pk):
     content = {}
     return render(request, 'basketapp/basket.html', content)
