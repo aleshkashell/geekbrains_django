6de48749a4e81ed1fbef2b7ebb7346a4ec20f830
Aleksey Sheludchenkov <aleshkashell@gmail.com>
2019-04-11 16:55:20 +0300

    ajax from guide

--- geekshop/basketapp/urls.py
+++ geekshop/basketapp/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', basketapp.basket, name='view'),
     path('add/<int:pk>/', basketapp.basket_add, name='add'),
     path('remove/<int:pk>)/', basketapp.basket_remove, name='remove'),
+    path('edit/<int:pk>/<int:quantity>/', basketapp.basket_edit, name='edit'),
 ]
--- geekshop/basketapp/views.py
+++ geekshop/basketapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.template.loader import render_to_string
+from django.http import JsonResponse
 from django.urls import reverse
 from basketapp.models import Basket
 from mainapp.models import Product
@@ -54,3 +56,22 @@ def basket_remove(request, pk):
     basket_record = get_object_or_404(Basket, pk=pk)
     basket_record.delete()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+@login_required
+def basket_edit(request, pk, quantity):
+    if request.is_ajax():
+        quantity = int(quantity)
+        new_basket_item = Basket.objects.get(pk=int(pk))
+        if quantity > 0:
+            new_basket_item.quantity = quantity
+            new_basket_item.save()
+        else:
+            new_basket_item.delete()
+        basket_items = Basket.objects.filter(user=request.user).order_by('product__category')
+        content = {
+            'basket_items': basket_items,
+        }
+        result = render_to_string('basketapp/includes/inc_basket_list.html',\
+                                   content)        
+        return JsonResponse({'result': result})
