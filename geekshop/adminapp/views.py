7843493b30e4904c057babf58de6fee4f5d15b7c
Aleksey Sheludchenkov <aleshkashell@gmail.com>
2019-04-14 14:44:35 +0300

    restrict access to admin panel

--- geekshop/adminapp/views.py
+++ geekshop/adminapp/views.py
@@ -4,8 +4,10 @@ from django.shortcuts import render
 from authapp.models import ShopUser
 from django.shortcuts import get_object_or_404, render
 from mainapp.models import Product, ProductCategory
+from django.contrib.auth.decorators import user_passes_test
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def users(request):
     title = 'админка/пользователи'
 
@@ -19,18 +21,22 @@ def users(request):
     return render(request, 'adminapp/users.html', content)
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def user_create(request):
     pass
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def user_update(request, pk):
     pass
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def user_delete(request, pk):
     pass
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def categories(request):
     title = 'админка/категории'
 
@@ -44,18 +50,22 @@ def categories(request):
     return render(request, 'adminapp/categories.html', content)
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def category_create(request):
     pass
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def category_update(request, pk):
     pass
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def category_delete(request, pk):
     pass
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def products(request, pk):
     title = 'админка/продукт'
 
@@ -71,17 +81,21 @@ def products(request, pk):
     return render(request, 'adminapp/products.html', content)
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def product_create(request, pk):
     pass
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def product_read(request, pk):
     pass
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def product_update(request, pk):
     pass
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def product_delete(request, pk):
     pass
