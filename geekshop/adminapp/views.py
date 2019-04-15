6c37d16639c7a5760e0272dba66a774d272fda9b
Aleksey Sheludchenkov <aleshkashell@gmail.com>
2019-04-15 14:22:23 +0300

    fix product edit

--- geekshop/adminapp/views.py
+++ geekshop/adminapp/views.py
@@ -112,6 +112,16 @@ class ProductUpdateView(UpdateView):
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
 
+    def get_success_url(self):
+        return reverse_lazy('adminapp:products',  kwargs={'pk': 0})
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Продукты'
+        category = {'pk': 0}
+        context['category'] = category
+        return context
+
 
 class ProductDeleteView(DeleteView):
     model = Product
