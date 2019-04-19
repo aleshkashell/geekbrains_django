fced717b80f6db80be64ab2f584bafe192b462f1
Aleksey Sheludchenkov <aleshkashell@gmail.com>
2019-04-19 14:09:23 +0300

    add debug toolbar

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -32,7 +32,8 @@ urlpatterns = [
     path('basket/', include('basketapp.urls', namespace='basket')),
     path('admin/', admin.site.urls),
     path('admin_custom/', include('adminapp.urls', namespace='admin_custom')),
-    path('social/', include('social_django.urls', namespace='social'))
+    path('social/', include('social_django.urls', namespace='social')),
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
 
 if settings.DEBUG:
